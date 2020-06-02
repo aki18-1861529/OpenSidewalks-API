1f50513e02f07d9228a1ef317a27d78622c2cce1
Alisha <43160608+unleeshthepower@users.noreply.github.com>
2020-06-02 15:00:05 -0700

    Add files via upload

--- workspace.py
+++ workspace.py
@@ -3,17 +3,44 @@ import numpy as np
 from shapely import geometry
 import networkx as nx
 import pyproj as proj
-import numexpr as num
+import numexpr as num 
+import pickle
+from bellevueWorkspace import create_graph_bell
 
 
-sdw = gp.read_file('Sidewalks/geo_export_8f5a3f72-4a94-4006-84e5-650856822b59.shp')
+bbox = (-97.74655,30.28317,-97.73650,30.26129)
+# Austin Data: ('Sidewalks/geo_export_8f5a3f72-4a94-4006-84e5-650856822b59.shp')
+# Bellevue Data: 'Bellevue_data/PedestrianFacilities_COBApr2020.shp'
+sdw = gp.read_file('Bellevue_data/PedestrianFacilities_COBApr2020.shp')
+
+if "pedestrian" in sdw.keys():
+    sdw_exists = sdw[sdw["pedestrian"] != 'ABSENT_SIDEWALKS']
+else:
+    sdw_exists = sdw
+
+# cast coordinates to projected system (meters)
+sdw_exists = sdw_exists.to_crs("EPSG:32633")
 
 
-sdw_exists = sdw[sdw["pedestrian"] != 'ABSENT_SIDEWALKS']
+# check if street name is provided in dataset
+street_name_exists = False
+if "full_stree" in sdw_exists.keys():
+    street_name_exists = True
+
+
 
 def create_graph(gdf, precision=1, simplify=0.05):
+
     # The geometries sometimes have tiny end parts - get rid of those!
     gdf.geometry = gdf.geometry.simplify(simplify)
+    
+
+    # Set street name 
+    if street_name_exists:
+        street_name = gdf.iloc[0,3]
+    else:
+        street_name = "null"
+
     G = nx.DiGraph()
 
 
@@ -24,40 +51,43 @@ def create_graph(gdf, precision=1, simplify=0.05):
     
  
     # Edges are stored as (from, to, data), where from and to are nodes.
-    # az1 is the azimuth of the first segment of the geometry (point into the
-    # geometry), az2 is for the last segment (pointing out of the geometry)
     def add_edges(row, G, precision=precision):
-        # setup your projections
-        # crs_wgs = proj.Proj(init='epsg:4326') # WGS84 geographic
-        # crs_bng = proj.Proj(init='epsg:102005')
-
         
         geom = row.geometry
-                
-
-        if geom.geom_type == "MultiLineString":
-            multicoords = [list(line.coords) for line in geom]
+        
+        if geom.geom_type == "MultiLineString" and street_name_exists:
+            multicoords = [list(line.coords) for line in geom] #gets coordinates of all linestrings
             # Making a flat list -> LineString
             simple = geometry.LineString([item for sublist in multicoords for item in sublist])
             coords = list(simple.coords)
+        elif geom.geom_type == "MultiLineString" and not street_name_exists:
+            for line in geom: #gets coordinates of all linestrings
+                coords = list(line.coords)
         elif geom.geom_type == "LineString":
             coords = geom.coords
+            # print("coords ", geom)
 
-        # cast coordinates to projected system
-        # for c in coords:
-            # c = proj.transform(crs_wgs, crs_bng, c[0], c[1])
+        
+
 
         geom = geometry.LineString(coords[::])
         geom_r = geometry.LineString(coords[::-1])
         start = make_node(coords[0], precision) # first element
         end = make_node(coords[-1], precision) # last element
 
+
+        # edge case - to avoid nodes with more than 4 degrees
+        if G.degree(nbunch=start) == 4:
+            start = make_node(coords[0], 3)
+        if G.degree(nbunch=end) == 4:
+            end = make_node(coords[-1], 3)
+
+
         # Add forward edge
         fwd_attr = {
             'forward': 1,
             'geometry': geom,
-            # 'az1': azimuth(coords[0], coords[1]),
-            # 'az2': azimuth(coords[-2], coords[-1]),
+            'street': street_name,
             'visited': 0,
         }
         G.add_edge(start, end, **fwd_attr)
@@ -66,8 +96,7 @@ def create_graph(gdf, precision=1, simplify=0.05):
         rev_attr = {
             'forward': 0,
             'geometry': geom_r,
-            # 'az1': azimuth(coords_r[0], coords_r[1]),
-            #'az2': azimuth(coords_r[-2], coords_r[-1]),
+            'street': street_name,
             'visited': 0,
         }
         G.add_edge(end, start, **rev_attr)
@@ -94,7 +123,7 @@ def create_graph(gdf, precision=1, simplify=0.05):
 
 
 def graph_workflow(gdf, precision=1):
-    grouped = gdf.groupby("full_stree")
+    grouped = gdf.groupby(["full_stree"], as_index=False)
     # undirected subgraphs for each street name
     subgraphs = []
 
@@ -110,46 +139,21 @@ def graph_workflow(gdf, precision=1):
 
 
 
-def merge_edges(G):
-    starting_nodes = []
-    newG = nx.DiGraph()
-    # make a list of starting nodes 
-    for n in G.nodes(G):
-        if G.degree(nbunch=n) == 2: # in-degree plus out-degree
-            starting_nodes.append(n)
-    # iterate through starting nodes to connect sidewalks
-    for node in starting_nodes:
-        geom_all = []
-        first_neigh = G.successors(node)
-        edge_data_1 = G.get_edge_data(node, first_neigh)
-        init_geom = edge_data_1[node][first_neigh]['geometry']
-        fwd = init_geom = edge_data_1[node][first_neigh]['forward']
-        geom_all.append(init_geom)
-        while G.degree(G, nbunch=first_neigh) != 2: #in-degree plus out-degree
-            second_neigh = G.successors(first_neigh)  # Returns an iterator over successor nodes of n.
-            for neigh in second_neigh:
-                if neigh is not first_neigh: # select node going in correct direction
-                    second_neigh = neigh 
-            edge_data_2 = G.get_edge_data(first_neigh, second_neigh)
-            next_geom = edge_data_2[first_neigh][second_neigh]['geometry']
-            geom_all.append(next_geom)
-            first_neigh = second_neigh
-        dict_info = {
-            'forward': fwd,
-            'geometry': geom_all,
-        }
-        newG.add_edge(node, first_neigh, dict_info)
-    return newG
-
 
 def main():
-    sgraphs = graph_workflow(sdw_exists)
-    for sg in sgraphs:
-       sg = merge_edges(sg)
-    # combined = nx.disjoint_union_all(sgraphs)
-    # new_gdf = nx.to_pandas_adjacency(combined)
-    # new_gdf.to_file("sdw_connect_test.shp")
-    print("complete!")
+    if street_name_exists:
+        sgraphs = graph_workflow(sdw_exists)
+    else:
+        sgraphs = []
+        sgraphs.append(create_graph_bell(sdw_exists, street_name_exists))
+    
+    with open('bellevue0602.pickle', 'wb') as f:
+    # Pickle the 'data' dictionary using the highest protocol available.
+        pickle.dump(sgraphs, f, pickle.HIGHEST_PROTOCOL)
+
+    
+
+    # for combining: seattle repo. mid-block crosswalk. where crossings get joined to sidewalks
 
 
 if __name__ == "__main__":
